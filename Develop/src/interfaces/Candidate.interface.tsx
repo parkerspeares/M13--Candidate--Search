21f9667c377693e7605e5d578ab0a856709bb98e
parkerspeares <parker.speares@outlook.com>
2024-11-26 13:47:31 -0800

    final restructure for deploy

--- Develop/src/interfaces/Candidate.interface.tsx
+++ Develop/src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,9 @@
-// TODO: Create an interface for the Candidate objects returned by the API
+export interface Candidate {
+    name: string;
+    login: string;
+    location?: string;  
+    avatar_url: string;
+    email?: string;     
+    html_url: string;
+    company?: string;   
+}
